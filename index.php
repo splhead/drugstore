af4be5e4c5f9c9bf9f44451d4002b158702703b1
Silas Pinho Ladislau <splhead@gmail.com>
2015-04-02 14:52:06 -0400

    teste

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all">	 
+	<meta name="title" content="Farmácia Escola">
+	<meta name="url" content="http://www.farmaciaescola.com.br">
+	<meta name="description" content="Farmácia Escola - Preparando o Profissional para a Farmácia">
+	<meta name="keywords" content="Farmácia Escola, Cursos de Capacitação em Rondônia, Aprendizado sobre medicamentos, Conhecimentos operacionais da drogaria, Curso de Técnicas de Vendas, Curso de Atendimento ao público, Treinamentos em rondônia, Curso de Relacionamento interpessoal, Curso com aulas práticas, Curso de Fidelização de clientes,Cursos de Comunicação, Curso de Marketing Pessoal">  
+	<meta name/="author" content="Farmácia Escola">
+	<meta name="company" content="Farmácia Escola">
+	<meta name="revisit-after" content="3">
+	<title>Farm&aacute;cia Escola</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body role="document">
+	<header>		
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div id="container">
+				<div id="navbar-header">
+					<a class="navbar-brand" href=""><img src="images/logo.png" alt="logo"/></a>
+					<ul class="nav navbar-nav">
+						<li><a href="#">IN&Iacute;CIO</a></li>
+						<li><a href="#">O CURSO</a></li>
+						<!--<li><a href="#">CONTE&Uacute;DO</a></li>-->
+						<li><a href="#">INSCRI&Ccedil;&Atilde;O</a></li>
+						<li><a href="#">DEPOIMENTOS</a></li>
+						<li><a href="#">&Aacute;REA DO ALUNO</a></li>
+						<li><a href="#">TALENTOS</a></li>
+						<li><a href="#">CONTATO</a></li>
+					</ul>
+				</div>
+			</div> 
+		</nav>
+	</header>
+	<div id="container">
+		
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+   
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="images/slide1.jpg" data-src="holder.js/1356x500/auto/#666:#444/text:img" alt="First slide">
+          </div>
+          <div class="item">
+            <img data-src="holder.js/1140x500/auto/#666:#444/text:Second slide" alt="Second slide">
+          </div>
+          <div class="item">
+            <img data-src="holder.js/1140x500/auto/#555:#333/text:Third slide" alt="Third slide">
+          </div>
+        </div>
+        
+      </div>
+
+	</div>
+	 <div class="container">
+	  <div class="row">
+	  	<div class="col-md-12">
+		  	<p ><h3><small class="text-uppercase text-center">farm&aacute;cia escola</small></h3></p>
+		  	<p ><h2 class="text-uppercase">curso de capacita&ccedil;&atilde;o</h2></p>
+		  	<p class="lead">Preparando o Profissional para a Farm&aacute;cia</p>
+	  	</div>
+	  </div>
+        <div id="#curso" class="col-md-4">
+          <h2 class="text-center">Objetivos</h2>
+          <p class="text-justify">Preparar o profissional para atuar no segmento farmacêutico, fornecendo noções sobre medicamentos, técnicas de venda modernas, atendimento ao público, relacionamento interpessoal e conhecimentos operacionais sobre o funcionamento de uma Drogaria.</p>
+        </div>
+        <div class="col-md-4">
+          <h2 class="text-center">P&uacute;blico Alvo</h2>
+          <p class="text-justify">Jovens e adultos a partir dos 16 anos, que já cursaram ou estão cursando o ensino médio, interessados em ingressar neste mercado, estudantes e profissionais da área de saúde e profissionais que já atuam na área e queiram se reciclar. O mercado de trabalho é bastante favorável, pois tanto as Drogarias de pequeno e médio porte, como as grandes redes valorizam o bom profissional.</p>
+       </div>
+        <div class="col-md-4">
+          <h2 class="text-center">Aulas Presenciais</h2>
+          <p class="text-justify">Aulas Práticas didáticas e com conteúdo de fácil entendimento.</p>          
+        </div>
+      </div>
+	<footer class="clearfix col-md-12 text-center"><h3><small>&copy; 2015 - Farm&aacute;cia Escola - Todos os direitos reservados.</small></h3></footer>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/docs.min.js"></script>
+</body>
+</html>
